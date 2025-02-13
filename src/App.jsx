d7e993b140839e654b85dc4a904d6b888ea687b8
JuanGaljoen <juangaljoen@gmail.com>
2025-02-13 15:54:34 +0200

    Cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
-import { useState } from 'react'
 import './App.css'
 import QuoteForm from './components/QuoteForm.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     < QuoteForm />
