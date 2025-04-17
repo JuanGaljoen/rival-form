1ef67815ab99cb3de58c33808ffb09eadefe8a61
JuanGaljoen <juangaljoen@gmail.com>
2025-04-17 12:56:01 +0200

    Removed unused imports

--- src/components/QuoteForm.jsx
+++ src/components/QuoteForm.jsx
@@ -4,10 +4,7 @@ import {
     Card,
     CardContent,
 } from "@/components/ui/card";
-import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
-import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
-
 import PowderForm from './PowderForm';
 import CapsuleForm from './CapsuleForm';
 import BasicDetailsForm from './BasicDetailsForm';
